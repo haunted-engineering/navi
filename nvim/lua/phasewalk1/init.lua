fdc49a425ccc872997812b07da10443225169812
phasewalk1 <staticanne@skiff.com>
2023-01-28 19:51:59 -0800

    rm nvim dir

--- nvim/lua/phasewalk1/init.lua
+++
@@ -1,8 +0,0 @@
-require("phasewalk1.set")
-require("phasewalk1.packer")
-require("phasewalk1.remap")
-require("phasewalk1.coc")
-require("phasewalk1.tree")
-require("phasewalk1.rust")
-require("phasewalk1.lualine")
-require("phasewalk1.transp")
--- nvim/lua/phasewalk1/tree.lua
+++
@@ -1,9 +0,0 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.opt.termguicolors = true
-
-require("nvim-web-devicons").setup()    
-
-require("nvim-tree").setup()
-
